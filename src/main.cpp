88156219f43cccb16615d68afec96fa803392223
Hans Otto Elneff <elneff@pm.me>
2023-10-07 16:10:34 +0300

    Comment to explain use of push/pop glstates funcs

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,12 @@ static void handleWindowEvents(sf::Window &window, bool &isAppRunning)
 
 static void renderImGui(sf::RenderWindow &window, sf::Time delta)
 {
+    // TODO: These push/pop operations are rather expensive to do every render cycle,
+    // so find/create an OpenGL backend for ImGui instead of the imgui-sfml library.
+    // Once this is done, change the SFML window back to a regular sf::Window.
+
+    // ImGui relies on SFML's graphics module, which might change OpenGL state,
+    // so we push and pop our state to preserve it.
     window.pushGLStates();
     ImGui::SFML::Update(window, delta);
     ImGui::ShowDemoWindow();
